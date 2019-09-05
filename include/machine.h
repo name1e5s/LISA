69799f59f256d9f7688e0f41a500372b9170e376
name1e5s <name1e5s@qq.com>
2019-09-06 00:51:32 +0800

    Test framework

---
+++ include/machine.h
@@ -0,0 +1,12 @@
+#ifndef __MACHINE
+#define __MACHINE
+
+#define LED_LO_ADDR         0xbfaf0000
+#define LED_HI_ADDR         0xbfaf0004
+#define NUM_ADDR            0xbfaf0008
+#define SWITCH_ADDR         0xbfaf000c    
+#define SWITCH_DIP_ADDR     0xbfaf0010
+#define VUART_ADDR          0xbfaf0014
+#define SWITCH_IN_ADDR      0xbfaf0018
+
+#endif
